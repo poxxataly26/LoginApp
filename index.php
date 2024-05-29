d8131b9da5e6e5b2b5cea5349794da9ba780326d
Talyta <talitaaugustoaraujo@gmail.com>
2024-05-29 08:04:55 -0300

    Inserindo espaçamento entre as linhas no index.php

--- index.php
+++ index.php
@@ -1,27 +1,23 @@
 <!doctype html>
 <html lang="en" class="h-100">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.84.0">
-    <title>Cover Template · Bootstrap v5.0</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cover/">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.84.0">
+  <title>Cover Template · Bootstrap v5.0</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cover/">
 
-    
-
-    <!-- Bootstrap core CSS -->
+  
+  <!-- Bootstrap core CSS -->
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <style>
-        /*
- * Globals
- */
 
 
-/* Custom default button */
+<style>
+
 .btn-secondary,
 .btn-secondary:hover,
 .btn-secondary:focus {
@@ -30,9 +26,7 @@
 }
 
 
-/*
- * Base structure
- */
+
 
 body {
   text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
@@ -42,11 +36,6 @@ body {
 .cover-container {
   max-width: 42em;
 }
-
-
-/*
- * Header
- */
 
 .nav-masthead .nav-link {
   padding: .25rem 0;
@@ -70,19 +59,19 @@ body {
   border-bottom-color: #fff;
 }
 .bd-placeholder-img {
-    font-size: 1.125rem;
-    text-anchor: middle;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    user-select: none;
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
 }
 
-    @media (min-width: 768px) {
-    .bd-placeholder-img-lg {
-        font-size: 3.5rem;
-    }
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
 }
-    </style>
+</style>
 
     <link href="" rel="stylesheet">
   </head>
@@ -108,17 +97,16 @@ body {
     <p class="lead">
       <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Github</a>
     </p>
-  </main>
+</main>
 
-  <footer class="mt-auto text-white-50">
-    <p>Modelo de capa do <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
-  </footer>
-  <footer class="text-white-50">
-    <p>Projeto criado por <a href="https://www.linkedin.com/feed/?trk=registration-frontend" class="text-white">@TalytaAugusto</a>.</p>
+<footer class="mt-auto text-white-50">
+  <p>Modelo de capa do <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+</footer>
+<footer class="text-white-50">
+  <p>Projeto criado por <a href="https://www.linkedin.com/feed/?trk=registration-frontend" class="text-white">@TalytaAugusto</a>.</p>
   </footer>
 </div>
 
 
-    
   </body>
 </html>
